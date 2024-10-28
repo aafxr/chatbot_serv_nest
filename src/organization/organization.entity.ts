569c40a282fa02df32df5efa831d11390e4ab99f
aafxr <alexandral.fed23@gmail.com>
2024-10-28 12:13:03 +0700

    organization service

--- src/organization/organization.entity.ts
+++ src/organization/organization.entity.ts
@@ -4,9 +4,22 @@ import { Column, PrimaryColumn } from 'typeorm';
 
 @Injectable()
 export class Organization {
+  constructor(o: Partial<Organization>) {
+    if(o.id) this.id = o.id
+    if(o.name) this.name = o.name
+    if(o.fullName) this.fullName = o.fullName
+    if(o.address) this.address = o.address
+    if(o.country) this.country = o.country
+    if(o.city) this.city = o.city
+    if(o.inn) this.inn = o.inn
+    if(o.userId) this.userId = o.userId
+    o.createdAt ? this.createdAt = new Date(o.createdAt) : this.createdAt = new Date()
+    if(o.updatedAt) this.updatedAt = new Date(o.updatedAt)
+    if(o.deletedAt) this.deletedAt = new Date(o.deletedAt)
+  }
 
   @PrimaryColumn()
-  id: number
+  id: number = 0
 
   @Column()
   name: string = ''
@@ -28,4 +41,13 @@ export class Organization {
 
   @Column()
   userId: User['id']
+
+  @Column()
+  createdAt: Date
+
+  @Column()
+  updatedAt: Date | null = null
+
+  @Column()
+  deletedAt: Date | null = null
 }
--- src/organization/organization.service.ts
+++ src/organization/organization.service.ts
@@ -1,13 +1,30 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 import { Organization } from './organization.entity';
+import { User } from '../user/user.entity';
 
 @Injectable()
 export class OrganizationService {
   constructor(
     @Inject('ORGANIZATION__REPOSITORY')
-    private readonly organizationRepository: Repository<Organization>
+    private readonly organizationRepository: Repository<Organization>,
   ) {}
 
+  async create(org: Organization){
+    return this.organizationRepository.save(org)
+  }
 
+  async getBtId(id: Organization['id']) {
+    return this.organizationRepository.findOneBy({id})
+  }
+  
+  async getAll(userId: User['id']){
+    return this.organizationRepository.find({ where: {userId, deletedAt: IsNull()} });
+  }
+
+  async delete(org: Organization){
+    const o = new Organization(org)
+    o.deletedAt = new Date()
+    return this.organizationRepository.save(o)
+  }
 }
